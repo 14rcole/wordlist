626b6616dea1122422c39505169a4bb76a4aac0f
daniel sohn <daniel sohn>
2015-11-30 12:10:30 -0500

    added words to list

--- shortestPath.h
+++
@@ -1,6 +0,0 @@
-#ifndef _shortestPath_h_
-#define _shortestPath_h_
-
-void aStarSearch(string begin, string end, string filename);
-int editDistance(string word1, string word2);
-#endif
--- two-words.py
+++
@@ -1,7 +0,0 @@
-import random, linecache
-
-lineNum1 = random.randrange(1, 2482)
-lineNum2 = random.randrange(1, 2482)
-
-print linecache.getline("four-letter-words", lineNum1)
-print linecache.getline("four-letter-words", lineNum2)
